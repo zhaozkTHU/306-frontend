8b4041c002ff301ae6533b0ad2e2e30c2bf1dfbd
张子颖 <790772462@qq.com>
2023-03-27 22:58:58 +0800

    FEAT: Add task block to provide interface
    We have a task list on the page and every task is a block
    We set every interface on the block, such as downloading data, updating the task and checking
    We add the task block to expose these interface

---
+++ src/components/check/checkModel.tsx
@@ -0,0 +1,20 @@
+import CheckTextClassificationTask from "./checkTextClassificationTask"
+
+interface CheckModelProps {
+    task_id: number,
+    labeler_id: number
+    template: string
+    isShow: boolean
+}
+
+const CheckModel = (props: CheckModelProps) => {
+    if(!props.isShow) {
+        return <p>error</p>
+    }
+    if(props.template==="TextClassification") {
+        return <CheckTextClassificationTask task_id={props.task_id} labeler_id={props.labeler_id}/>
+    }
+    return <p>error</p>
+}
+
+export default CheckModel
