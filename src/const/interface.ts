ca076215547001c817b118ec3ae59c0eb2cc6a14
赵泽锴 <2021010731@secoder.net>
2023-04-08 10:09:24 +0800

    FEAT: Add new problems interface

--- src/const/interface.ts
+++ src/const/interface.ts
@@ -11,8 +11,42 @@ export interface TextClassificationProblem {
 
 export interface ImagesClassificationProblem {
   description: string;
+  /** 图片url */
   options: string[];
   chosen?: boolean[];
+}
+
+export interface FaceTagProblem {
+  faceImageUrl: string;
+  /** 点坐标数组 */
+  data?: [number, number][];
+}
+
+export interface ImageFramePromblem {
+  imageUrl: string;
+  /** 图片框选矩形，左下和右上确定矩形 */
+  data?: {
+    leftdowm: [number, number];
+    rightup: [number, number];
+  }[];
+}
+
+export interface SoundTagProblem {
+  soundUrl: string;
+  description: string;
+  choice: {
+    text: string;
+    needInput: boolean;
+  }[];
+  data?: {
+    choiceIndex: number;
+    input?: string;
+  };
+}
+
+// 将soundUrl 改为 videoUrl
+export interface VideoTagProblem extends Omit<SoundTagProblem, "soundUrl"> {
+  videoUrl: string;
 }
 
 export interface TaskInfo {
@@ -20,12 +54,22 @@ export interface TaskInfo {
   title: string;
   create_at: number;
   deadline: number;
-  template: "TextClassification" | "ImagesClassification";
+  template:
+    | "TextClassification"
+    | "ImagesClassification"
+    | "FaceTag"
+    | "ImageFrame"
+    | "SoundTag"
+    | "VideoTag";
   reward: number;
   time: number;
   labeler_number: number;
   demander_id?: number;
-  task_data: TextClassificationProblem[] | ImagesClassificationProblem[];
+  task_data:
+    | TextClassificationProblem[]
+    | ImagesClassificationProblem[]
+    | FaceTagProblem[]
+    | ImageFramePromblem[];
 }
 
 export interface TextClassificationData {
