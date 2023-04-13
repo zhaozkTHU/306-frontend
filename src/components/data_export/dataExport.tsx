6c2a44a9065cae9b9cbf21d248d8ed65df4deb26
赵泽锴 <2021010731@secoder.net>
2023-04-13 18:15:27 +0800

    FIX: DataExportCallback is an async function now

--- src/components/data_export/dataExport.tsx
+++ src/components/data_export/dataExport.tsx
@@ -5,39 +5,33 @@ import axios from "axios";
  * 导出数据的回调函数
  * @param taskId 任务id
  * @param merge 是否合并数据
+ * @throws 错误信息，应该是`string`
  */
-export const DataExportCallback = (taskId: number, merge: boolean) => {
-  axios
-    .post(
-      "/api/data",
-      { task_id: taskId, merge: merge },
-      {
-        headers: {
-          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
-        },
-      }
-    )
-    .then((value) => {
-      if (value.data.code === 0) {
-        const jsonData = JSON.stringify(value.data.data);
-        const blob = new Blob([jsonData], { type: "application/json" });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.download = "data.json";
-        a.href = url;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        URL.revokeObjectURL(url);
-        message.success("导出成功");
-      } else {
-        message.error(`导出失败 ${value.data?.message}`);
-      }
-    })
-    .catch((reason) => {
-      console.log(reason);
-      message.error(`网络错误 ${reason?.response?.data?.message}`);
+const DataExportCallback = async (taskId: number, merge: boolean) => {
+  let value;
+  try {
+    value = await axios.get("/api/data", {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      params: { task_id: taskId, merge },
     });
+  }
+  catch (reason) {
+    throw new Error((reason as any)?.data?.message);
+  }
+  if (value.data.code === 0) {
+    const jsonData = JSON.stringify(value.data.data);
+    const blob = new Blob([jsonData], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.download = "data.json";
+    a.href = url;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+  } else {
+    throw new Error(value.data?.message);
+  }
 };
 
 export default DataExportCallback;
