ce262a8c8e4771695ad3260954d62a26cba28481
zoumh21 <zerozmh030917@163.com>
2023-04-25 18:40:29 +0800

    REFACTOR: sound&video tag and classification & FIX: local storage

--- src/const/interface.ts
+++ src/const/interface.ts
@@ -23,7 +23,7 @@ export interface FaceTagProblem {
   data?: [number, number][];
 }
 
-export interface ImageFramePromblem {
+export interface ImageFrameProblem {
   description: string;
   url: string;
   /** 图片框选矩形，左下和右上确定矩形 */
@@ -70,12 +70,51 @@ export interface TaskInfo {
     | TextClassificationProblem[]
     | ImagesClassificationProblem[]
     | FaceTagProblem[]
-    | ImageFramePromblem[]
+    | ImageFrameProblem[]
     | TagProblem[];
 }
 
 export interface TextClassificationData {
   label_data: boolean[][];
+}
+
+export function isFaceTagProblem(data: any): data is FaceTagProblem {
+  return (
+    typeof data.description === "string" &&
+    typeof data.url === "string" &&
+    (data.data === undefined ||
+      (Array.isArray(data.data) &&
+        data.data.every(
+          (point: any) =>
+            Array.isArray(point) &&
+            point.length === 2 &&
+            typeof point[0] === "number" &&
+            typeof point[1] === "number"
+        )))
+  );
+}
+
+export function isImageFrameProblem(data: any): data is ImageFrameProblem {
+  return (
+    typeof data.description === "string" &&
+    typeof data.url === "string" &&
+    (data.data === undefined ||
+      (Array.isArray(data.data) &&
+        data.data.every((rect: any) => {
+          return (
+            typeof rect.leftdown === "object" &&
+            Array.isArray(rect.leftdown) &&
+            rect.leftdown.length === 2 &&
+            typeof rect.leftdown[0] === "number" &&
+            typeof rect.leftdown[1] === "number" &&
+            typeof rect.rightup === "object" &&
+            Array.isArray(rect.rightup) &&
+            rect.rightup.length === 2 &&
+            typeof rect.rightup[0] === "number" &&
+            typeof rect.rightup[1] === "number"
+          );
+        })))
+  );
 }
 
 export function isClassificationProblem(data: any): data is TextClassificationProblem {
