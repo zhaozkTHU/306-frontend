129a426c0ca67eb155aa614437480ce08083a81a
zoumh21 <zerozmh030917@163.com>
2023-05-10 12:22:02 +0800

    FEATURE: vip time add

---
+++ src/pages/test/trybd.tsx
@@ -0,0 +1,15 @@
+import ImageAnnotation from "../../components/task_label/MyAnnotator";
+const Try_BD = () => {
+
+    return 
+    <p>
+        {/* <ImageAnnotation 
+            src={}
+            onChange={}
+            tools={"dot"}
+            initialAnnotations={[]}
+        /> */}
+    </p>;
+};
+  
+export default Try_BD;
