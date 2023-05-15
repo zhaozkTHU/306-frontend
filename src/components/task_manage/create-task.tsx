8ae75b18db32ea9b397834257a82fe98d0a2cbaf
赵泽锴 <2021010731@secoder.net>
2023-05-15 11:44:15 +0800

    FIX: not reset form when request failed now

--- src/components/task_manage/create-task.tsx
+++ src/components/task_manage/create-task.tsx
@@ -9,24 +9,28 @@ import axios from "axios";
  * @returns 创建任务组件
  */
 const CreateTask: React.FC = () => {
-  const onFinish = (info: TaskInfo) => {
-    axios
-      .post(
-        "/api/task",
-        { ...info, create_at: new Date().valueOf() },
-        {
-          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
-        }
-      )
-      .then((value) => {
-        if (value.data.code === 0) message.success("发布成功");
-        else message.error(value.data.reason);
-      })
-      .catch((reason) => {
-        console.log(reason);
-        message.error(`网络错误 ${reason?.response?.data?.message}`);
-      });
-  };
+  const onFinish = (info: TaskInfo): Promise<void> =>
+    new Promise((resolve, reject) => {
+      axios
+        .post(
+          "/api/task",
+          { ...info, create_at: new Date().valueOf() },
+          {
+            headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+          }
+        )
+        .then((value) => {
+          if (value.data.code === 0) {
+            message.success("发布成功");
+            resolve();
+          } else message.error(value.data.reason);
+        })
+        .catch((reason) => {
+          console.log(reason);
+          message.error(`网络错误 ${reason?.response?.data?.message}`);
+          reject();
+        });
+    });
 
   return <TaskInfoForm taskInfo={{} as TaskInfo} onFinish={(info: TaskInfo) => onFinish(info)} />;
 };
